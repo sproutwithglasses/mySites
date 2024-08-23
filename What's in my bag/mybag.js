b466c899e7422665214b6ec1152780c030cb6155
sproutwithglasses <splittingtomato@gmail.com>
2024-08-23 05:59:27 -0400

    Updates to What's in my Bag 1.0
    Basically the first full rendition of the website is done. Decorative and more css to be added from here on out.
    
    1.0
    -fixed hover and image placements within the hover box
    -hover and itemdes added
    -new images and editing done
    -z-index with jquery stacking added
    
    - work on media query and how it presents on other devices
    - consider a background
    - consider a large decorative title

---
+++ What's in my bag/mybag.js
@@ -0,0 +1,111 @@
+ //book
+ $("#hoverbook").hover(function(){
+    $('.book').removeClass('hidden');
+},function(){
+    $('.book').addClass('hidden');
+});
+
+//headphones
+$("#hoverheadphones").hover(function(){
+    $('.headphones').removeClass('hidden');
+},function(){
+    $('.headphones').addClass('hidden');
+});
+
+//lighter
+$("#hoverlighter").hover(function(){
+    $('.lighter').removeClass('hidden');
+},function(){
+    $('.lighter').addClass('hidden');
+});
+
+//wallet
+$("#hoverwallet").hover(function(){
+    $('.wallet').removeClass('hidden');
+},function(){
+    $('.wallet').addClass('hidden');
+});
+
+//keys
+$("#hoverkeys").hover(function(){
+    $('.keys').removeClass('hidden');
+},function(){
+    $('.keys').addClass('hidden');
+});
+
+//cube
+$("#hovercube").hover(function(){
+    $('.cube').removeClass('hidden');
+},function(){
+    $('.cube').addClass('hidden');
+});
+
+//aquaphor
+$("#hoveraquaphor").hover(function(){
+    $('.aquaphor').removeClass('hidden');
+},function(){
+    $('.aquaphor').addClass('hidden');
+});
+
+//facecushion
+$("#hoverfacecushion").hover(function(){
+    $('.facecushion').removeClass('hidden');
+},function(){
+    $('.facecushion').addClass('hidden');
+});
+
+//glasses
+$("#hoverglasses").hover(function(){
+    $('.glasses').removeClass('hidden');
+},function(){
+    $('.glasses').addClass('hidden');
+});
+
+//perfume
+$("#hoverperfume").hover(function(){
+    $('.perfume').removeClass('hidden');
+},function(){
+    $('.perfume').addClass('hidden');
+});
+
+//harddrive
+$("#hoverharddrive").hover(function(){
+    $('.harddrive').removeClass('hidden');
+},function(){
+    $('.harddrive').addClass('hidden');
+});
+
+//phone
+$("#hoverphone").hover(function(){
+    $('.phone').removeClass('hidden');
+},function(){
+    $('.phone').addClass('hidden');
+});
+
+//keychain
+$("#hoverkeychain").hover(function(){
+    $('.keychain').removeClass('hidden');
+},function(){
+    $('.keychain').addClass('hidden');
+});
+
+//camera
+$("#hovercamera").hover(function(){
+    $('.camera').removeClass('hidden');
+},function(){
+    $('.camera').addClass('hidden');
+});
+
+//ashwagandha
+$("#hoverashwagandha").hover(function(){
+    $('.ashwagandha').removeClass('hidden');
+},function(){
+    $('.ashwagandha').addClass('hidden');
+});
+
+//trinket
+$("#hovertrinket").hover(function(){
+    $('.trinket').removeClass('hidden');
+},function(){
+    $('.trinket').addClass('hidden');
+});
